4a51ab5a80fd2e0e1d89b6c02815316dbcc84134
indexquant <jakechoi@stanford.edu>
2021-11-01 16:26:24 +0800

    commit test

--- datastructure.py
+++ datastructure.py
@@ -1,6 +1,30 @@
-#Data Structures and Algorithms
+"""
+Python Self Cookbook
+
+last update : 1 Nov 2021
+The curriculum and script ideas are mainly sourced from "Python Cookbook by David Beazley & Brian K.Jones (O'Reilly)
+================================================================
+Table of Contents (# of sub-topics):
+
+1. Data Structures and Algorithms (20)
+2. Strings and Text (20)
+3. Numbers, Dates, and Times (16)
+4. Iterators and Generators (16)
+5. Files and I/O (21)
+6. Data Encoding and Processing (13)
+7. Functions (12)
+8. Classes and Objects (25)
+
+Some advanced topics may be included:
+Metaprogramming, Modules and Packages, Network and Web Programming, Concurrency, Utility Scripting and System Admin, Testing/Debugging/Exceptions, C Extension
+================================================================
+"""
 
 """
+================================================================
+Data Structures and Algorithms
+================================================================
+
 ============================================
 1. Unpacking a Sequence into Separate Variables
 ============================================
