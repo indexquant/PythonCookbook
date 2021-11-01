87dff3f4c2266962f9278379bd84d3833c82e6b3
indexquant <jakechoi@stanford.edu>
2021-11-01 16:03:42 +0800

    commit test

--- datastructure.py
+++ datastructure.py
@@ -3,3 +3,8 @@
 """
 Unpacking a Sequence into Separate Variables
 """
+#Any sequence can be unpacked into variables using a simple assignment operation'
+raw_data = ['TSLA', 1000, (2021, 11,1)]
+instrument_name, price, (year, month, day) = raw_data
+print(price)
+print(month)
