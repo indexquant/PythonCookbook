70cfc956cd5e1b0399c1765ce8ffc26f736b5c3d
indexquant <jakechoi@stanford.edu>
2021-11-01 16:31:30 +0800

    commit test

--- datastructure.py
+++ datastructure.py
@@ -22,13 +22,13 @@ Metaprogramming, Modules and Packages, Network and Web Programming, Concurrency,
 
 """
 ================================================================
-Data Structures and Algorithms
-================================================================
-
-================================================================
-1. Unpacking a Sequence into Separate Variables
+1. Data Structures and Algorithms
 ================================================================
 """
+
+
+######### Unpacking a Sequence into Separate Variables #########
+================================================================
 #Any sequence can be unpacked into variables using a simple assignment operation'
 raw_data = ['TSLA', 1000, (2021, 11,1)]
 instrument_name, price, (year, month, day) = raw_data
