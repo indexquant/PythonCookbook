992946155971b277927d4deafd879582625ea728
indexquant <jakechoi@stanford.edu>
2021-11-03 12:01:04 +0800

    added util.py

---
+++ util.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rcParams['font.family'] = 'serif'
+
+import math
+from scipy.integrate import quad
+
+
+##Helper Functions##
+
+def BSM_call_value():
+    print("imported.")
+
+
